a6a4f1b235164d019d5cf07b147e2c28688fd883
Jacob <jacob321williams@gmail.com>
2019-04-13 12:14:24 -0700

    OH GOD THE CODE

--- car/car.py
+++ car/car.py
@@ -1,6 +1,40 @@
 import random
 import time
 
+
+class intersection:
+    def __init__(self, xCoord, yCoord):
+        intersections.append(self)
+        self.xCoord = xCoord
+        self.yCoord = yCoord
+        
+        self.time = 0
+        
+        #allows cars from (no direction: -1, left: 1, up: 2, right: 3, down: 4) to procede
+        self.allowance = -1
+        #selects the next open road
+        self.next = 1
+
+    def changeLane(self):
+        self.time = 0
+        
+        self.allowance = self.next
+        self.next = self.next + 1 if self.next < 4 else 1
+        
+    def checkLane(self):
+        return self.allowance
+    
+    def drawIntersection(self):
+        self.time += 1
+        
+        if self.allowance == -1 and self.time > 30:
+            self.changeLane()
+        elif self.time > 90:
+            self.allowance = -1
+            self.time = 0
+    
+
+
 class Car:
 
     def __init__(self):
@@ -9,6 +43,8 @@ class Car:
 
         self.positionX = 0
         self.positionY = 0
+        
+        self.speed = 1
 
         if self.direction == 0:
             self.positionY = 0
@@ -34,36 +70,40 @@ class Car:
         
         return True
         
-    def collisionAvoidance(self):
-        potentialX = self.positionX
-        potentialY = self.positionY
+    def collisionAvoidance(self, posX, posY):
         
-        if self.direction == 0:
-            potentialY += 1
-        elif self.direction == 1:
-            potentialY -= 1
-        elif self.direction == 2:
-            potentialX += 1
-        elif self.direction == 3:
-            potentialX -= 1
+        for intersection in intersections:
+            if abs(intersection.xCoord - posX) < 5 and abs(intersection.yCoord - posY) < 5:
+                if (self.direction != intersection.checkLane()):
+                    self.speed = acceleration
+                    return False
+                
         
         for car in cars:
-            if car != self and abs(car.positionX - potentialX) < 3 and abs(car.positionY - potentialY) < 3:
+            if car != self and abs(car.positionX - posX) < 3 and abs(car.positionY - posY) < 3:
+                self.speed = acceleration
                 return False
-            
+        if self.speed < topSpeed:
+            self.speed += acceleration
+            self.speed = self.speed if self.speed < topSpeed else topSpeed   
+        
+         
         return True
 
     def move(self):
         
-        if self.collisionAvoidance():
-            if self.direction == 0:
-                self.positionY += 1
-            elif self.direction == 1:
-                self.positionY -= 1
-            elif self.direction == 2:
-                self.positionX += 1
-            elif self.direction == 3:
-                self.positionX -= 1
+        if self.direction == 0:
+            if self.collisionAvoidance(self.positionX, self.positionY + self.speed):
+                self.positionY += self.speed
+        elif self.direction == 1:
+            if self.collisionAvoidance(self.positionX, self.positionY - self.speed):
+                self.positionY -= self.speed
+        elif self.direction == 2:
+            if self.collisionAvoidance(self.positionX + self.speed, self.positionY):
+                self.positionX += self.speed
+        elif self.direction == 3:
+            if self.collisionAvoidance(self.positionX - self.speed, self.positionY):
+                self.positionX -= self.speed
 
         if self.positionY > 100 or self.positionY < 0  or self.positionX > 100 or self.positionX < 0 :
             cars.remove(self)
@@ -76,20 +116,32 @@ def setup():
     size(700, 700)
     frameRate(30)
 
+intersections = []
 cars = []
+acceleration = .1
+topSpeed = 1
+
+intersection(33,33)
+intersection(33,66)
+intersection(66,33)
+intersection(66,66)
 
 def draw():
     
     background(11, 102, 35)
-    carsAmount = 80
+    carsAmount = 40
     
     
 
     if len(cars) < carsAmount:
         Car()
 
+    for intersection in intersections:
+        print("Intersection allowance: " + str(intersection.allowance))
+        intersection.drawIntersection()
+
     for car in cars:
-        print(" car position is: " + str(car.positionX) + ", " + str(car.positionY))
+        #print(" car position is: " + str(car.positionX) + ", " + str(car.positionY))
         car.drawCar()
         car.move()
     #time.sleep(4)
